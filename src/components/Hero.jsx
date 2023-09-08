58b38314fd955a436dd46929840a998f5d4883ea
Jack Ball <ball.jack2376@gmail.com>
2023-09-08 11:56:47 -0600

    finished bootstrappinngg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
+import { Container, Col, Row } from "react-bootstrap";
+
 export default function Hero({ title, lead }) {
   return (
-    <section>
-      <h1>{title}</h1>
-      <p>{lead}</p>
-    </section>
+    <Container fluid className="text-center py-5 bg-light">
+      <Row className="py-lg-5">
+        <Col lg={6} md={8} className="mx-auto">
+          <h1 className="fw-light">{title}</h1>
+          <p className="lead text-muted">{lead}</p>
+        </Col>
+      </Row>
+    </Container>
   );
 }
--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -1,19 +1,32 @@
+import { Card, Container, Row, Col } from "react-bootstrap";
+
 function Value({ title, description, action }) {
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <p>{action}</p>
-    </div>
+    <Card className="card shadow-sm h-100">
+      <Card.Body className="p-4 d-flex flex-column">
+        <h2 className="card-title">{title}</h2>
+        <p className="card-text">{description}</p>
+        <a className="btn btn-success">{action}</a>
+      </Card.Body>
+    </Card>
   );
 }
 
 export default function Values({ values }) {
   return (
-    <section>
-      {values.map(({ title, description, action }, index) => (
-        <Value key={index} title={title} description={description} action={action} />
-      ))}
-    </section>
+    <Container fluid>
+      <Row>
+        {values.map(({ title, description, action }, index) => (
+          <Col key={title}>
+            <Value
+              key={index}
+              title={title}
+              description={description}
+              action={action}
+            />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
